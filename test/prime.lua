9dad5b00cf9ef9f8f2da41f7045bd631af82b0fa
hshibuya <goe@fuzz.co.jp>
2016-04-22 06:09:35 +0900

    optimize lua script

--- test/prime.lua
+++ test/prime.lua
@@ -1,16 +1,20 @@
 -- prime.lua
-function isprime(n, primes)
-    nsqrt = n ^ 0.5
-    for i = 1, #primes do
+function isprime(n, primes, primes_len)
+    local nsqrt = n ^ 0.5
+    for i = 1, primes_len do
         if nsqrt < primes[i] then break end
         if n % primes[i] == 0 then return false end
     end
     return true
 end
 
-primes = {}
+local primes = {}
+local primes_len = 0
 for i = 2, arg[1] * 1 do
-    if isprime(i, primes) then primes[#primes + 1] = i end
+    if isprime(i, primes, primes_len) then
+        primes_len = primes_len + 1
+        primes[primes_len] = i
+    end
 end
 
-print(primes[#primes])
+print(primes[primes_len])
